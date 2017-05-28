23d33f1a649916fcb471a8195ddddf5dd9f7ffc3
Kavinda Silva <kss.kavinda@gmail.com>
2017-05-28 11:28:54 +0530

    step 1 created for admin only

---
+++ admin/in2.php
@@ -0,0 +1,21 @@
+<?php 
+require_once '../dbcon.php';
+//session maintainence
+?>
+
+
+			<!--end of the main page-->
+		</div>
+
+		<!--start of the footer-->
+		<div id="foot" style="">
+			<center class="footfix">
+				&copy;KS <a href="http://ugvle.ucsc.cmb.ac.lk">visit us</a>
+			</center>
+		</div>
+		<!--end of the footer-->
+		
+		<div></div>
+	</body>
+
+</html>
